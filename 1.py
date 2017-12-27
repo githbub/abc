fb3c28526fa0e86a5880c9da3d9ddfd4ed13b3c7
siddht5 <dhar.75.ap@outlook.in>
2017-12-27 11:28:20 +0530

    Create 1.py

---
+++ 1.py
@@ -0,0 +1,14 @@
+import csv
+ 
+rows = [
+    {'name': u'Anton\xedn Dvo\u0159\xe1k','country': u'\u010cesko'},
+    {'name': u'Bj\xf6rk Gu\xf0mundsd\xf3ttir', 'country': u'\xcdsland'},
+    {'name': u'S\xf8ren Kierkeg\xe5rd', 'country': u'Danmark'}
+    ]
+ 
+out = open('test.csv', 'w')
+writer = csv.DictWriter(out, fieldnames=['name', 'country'])
+writer.writeheader()
+for row in rows:
+    writer.writerow( dict( (k, v.encode('utf-8')) for k, v in row.items()))
+out.close()
